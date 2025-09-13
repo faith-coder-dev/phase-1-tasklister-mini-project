9e633de548289432a3cf745b3b155bf5df444243
Faith Nkirote <nkirotef054@gmail.com>
2025-09-13 16:35:07 +0300

    task creation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+let form;
+let input;
+let taskList;
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form = document.getElementById("create-task-form");
+  input = document.getElementById("new-task-description");
+  taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+    let task = input.value.trim();
+    if (task) {
+      addTaskToDOM(task);
+      input.value = ""; 
+    }
+  });
 });
+
+function addTaskToDOM(task) {
+  let li = document.createElement("li");
+  li.textContent = task;
+
+  
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "x";
+  deleteBtn.style.marginLeft = "10px";
+
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
+  });
+
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+}
